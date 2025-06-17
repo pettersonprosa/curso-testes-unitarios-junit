7374fe57ad267c6d12f0877c062c8c1b2dbb684e
petterson <petterson@email.com>
2025-06-17 08:58:56 -0300

    Aplicando a nomenclatura do BDD para nomear métodos de teste

--- src/test/java/com/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/junit/utilidade/FiltroNumerosTest.java
@@ -11,8 +11,10 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FiltroNumerosTest {
 
+    //Given, When, Then
+    //Dado, Quando, Entao
     @Test
-    public void deve_Retornar_Numeros_Pares() {
+    public void Dado_uma_lista_de_numeros_pares_Quando_filtrada_por_pares_Entao_deve_retornar_apenas_numeros_pares() {
         // Cria uma lista de números inteiros para teste (1, 2, 3, 4)
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
         
@@ -27,4 +29,22 @@ public class FiltroNumerosTest {
         Assertions.assertIterableEquals(numerosParesEsperados, resultadoFiltro);
     }
 
+    @Test
+    public void Dado_uma_lista_de_numeros_pares_Quando_filtrada_por_impares_Entao_deve_retornar_apenas_numeros_impares() {
+        // Cria uma lista de números inteiros para teste (1, 2, 3, 4)
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        
+        // Define o resultado esperado após a filtragem (apenas números impares: 1, 3)
+        List<Integer> numerosImparesEsperados = Arrays.asList(1, 3);
+        
+        // Chama o método que está sendo testado (numerosImpares) passando a lista de números
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosImpares(numeros);
+        
+        // Verifica se o resultado da filtragem corresponde ao esperado
+        // assertIterableEquals é usado para comparar coleções (List, Set, etc.)
+        Assertions.assertIterableEquals(numerosImparesEsperados, resultadoFiltro);
+    }
+
+    
+    
 }
