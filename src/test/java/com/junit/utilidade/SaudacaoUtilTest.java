097703c4261b6b67d6abe8e187afb8992555b28f
petterson <petterson@email.com>
2025-06-23 18:11:04 -0300

    Testes parametrizados

--- src/test/java/com/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/junit/utilidade/SaudacaoUtilTest.java
@@ -6,9 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Testes no utilitário de saudação")
 public class SaudacaoUtilTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {5,6,7,8,9,10,11})
+    public void Dado_horario_matinal_Quando_saudar_Entao_deve_retornar_bom_dia(int hora) {
+        String saudacao = SaudacaoUtil.saudar(hora);
+
+        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    }
 
     @Test
     @DisplayName("Deve saldar com bom dia")
